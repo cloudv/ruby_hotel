d36e3cdceb2a2d0302fba8e35099581fbcac90c3
Yisen Zhang <yszhang@CNyszhang.local>
2014-01-23 22:26:33 +0800

    extract getResult method

--- spec/hotel_spec.rb
+++ spec/hotel_spec.rb
@@ -1,30 +1,38 @@
 require '../src/hotel'
 
 describe Hotel do
-  #before do
-  #  hotel = Hotel.new
-  #end
+  before do
+    @hotel = Hotel.new
+    @input1 = "Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"
+    @input2 = "Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)"
+    @input3 = "Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"
+    @days = ["mon","tues","wed"]
+  end
+
+
+  it 'shoudl return "330" when name is Lakewood and customerType "Regular" days["mon","tues","wed"]' do
+    @hotel.generateTotalPrice("Lakewood","Regular",["mon","tues","wed"]).should eq 330
+  end
+
+  it 'should return customerType "Regular" and orders "mon,tues,wed" when input "Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"' do
+    orders = @hotel.getOrders(@input1)
+    orders[:customerType].should eq "Regular"
+    orders[:days].should eq ['mon','tues','wed']
+  end
 
   it 'should return "Lakewood" when input "Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"' do
-    hotel = Hotel.new
-    hotel.getResult("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)").should eq "Lakewood"
+    @hotel = Hotel.new
+    @hotel.getResult(@input1).should eq "Lakewood"
   end
 
   it 'should return "Bridgewood" when input "Regular: 20Mar2009(fri),21Mar2009(sat),22Mar2009(sun)"' do
-    hotel = Hotel.new
-    hotel.getResult("Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)").should eq "Bridgewood"
+    @hotel = Hotel.new
+    @hotel.getResult(@input2).should eq "Bridgewood"
   end
 
   it 'should return "Ridgewood" when input "Rewards: 26Mar2009(thur),27Mar2009(fn),28Mar2009(sat):' do
-    hotel = Hotel.new
-    hotel.getResult("Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)").should eq "Ridgewood"
-  end
-
-  it 'should put a' do
-    hotel = Hotel.new
-    #hotel.stub(:fake)
-    hotel.stub(:afn).and_return("a")
-    hotel.fake
+    @hotel = Hotel.new
+    @hotel.getResult(@input3).should eq "Ridgewood"
   end
 
 end
--- src/hotel.rb
+++ src/hotel.rb
@@ -1,51 +1,116 @@
 class Hotel
 
-  def fake
-    afn(1)
+  attr_accessor :regular_week_price
+  attr_accessor :regular_weekend_price
+  attr_accessor :rewards_week_price
+  attr_accessor :rewards_weekend_price
+  attr_accessor :rate
+  attr_accessor :name
+
+
+  def getOrders orderInfo
+    /(Regular|Rewards).*\((\w{3,})\).*\((\w{3,})\).*\((\w{3,})\)/.match(orderInfo)
+    orders = {:customerType => $1, :days => [].push($2).push($3).push($4)}
   end
 
-  def afn arg
-    "a"
+  def generateTotalPrice name, customerType, days
+    price = 0
+    days.each{ |day|
+      if name.eql?("Lakewood")
+        if day.eql?("sat") || day.eql?("sun")
+          if (customerType.eql?("Regular"))
+            price += 90
+          else
+            price += 80
+          end
+        else
+          if (customerType.eql?("Regular"))
+            price += 110
+          else
+            price += 80
+          end
+        end
+      elsif name.eql?("Bridgewood")
+        if day.eql?("sat") || day.eql?("sun")
+          if (customerType.eql?("Regular"))
+            price += 60
+          else
+            price += 50
+          end
+        else
+          if (customerType.eql?("Regular"))
+            price += 160
+          else
+            price += 110
+          end
+        end
+      elsif name.eql?("Ridgewood")
+        if day.eql?("sat") || day.eql?("sun")
+          if (customerType.eql?("Regular"))
+            price += 150
+          else
+            price += 100
+          end
+        else
+          if (customerType.eql?("Regular"))
+            price += 220
+          else
+            price += 40
+          end
+        end
+      end
+    }
+    return price
   end
 
   def getResult orderInfo
-    /(Regular|Rewards).*\((\w{3,})\).*\((\w{3,})\).*\((\w{3,})\)/.match(orderInfo)
-    customerType = $1;
-    days = [].push($2).push($3).push($4)
+    orders = getOrders(orderInfo)
+    customerType = orders[:customerType]
+    days = orders[:days]
     lakewood = bridgewood = ridgewood =0
-    if customerType.eql?("Regular")
-      days.each{ |day|
-        if day.eql?("sat") || day.eql?("sun")
-                  lakewood += 90
-                  bridgewood += 60
-                  ridgewood += 150
-                else
-                  lakewood += 110
-                  bridgewood += 160
-                  ridgewood += 220
-                end
-      }
-    elsif customerType.eql?("Rewards")
-      days.each{ |day|
-        if day.eql?("sat") || day.eql?("sun")
-                  lakewood += 80
-                  bridgewood += 50
-                  ridgewood += 100
-                else
-                  lakewood += 80
-                  bridgewood += 110
-                  ridgewood += 40
-                end
-      }
-    end
-    temp = (lakewood < bridgewood)?lakewood : bridgewood
-    totalPrice = temp < ridgewood ? temp : ridgewood
-    if totalPrice.eql?(lakewood)
-      "Lakewood"
-    elsif totalPrice.eql?(bridgewood)
-      "Bridgewood"
-    elsif totalPrice.eql?(ridgewood)
-      "Ridgewood"
-    end
+    hotels = ["Lakewood","Bridgewood","Ridgewood"]
+    index = 0
+    temp = 0
+    hotels.map{ |hotel|
+      index += 1
+      price = generateTotalPrice(hotel,customerType,days)
+      temp = index if temp > price || temp = 0
+      {:name => hotel, :price => price}
+    }
+    hotels[index][:name]
+    #if customerType.eql?("Regular")
+    #  days.each { |day|
+    #    if day.eql?("sat") || day.eql?("sun")
+    #      lakewood += 90
+    #      bridgewood += 60
+    #      ridgewood += 150
+    #    else
+    #      lakewood += 110
+    #      bridgewood += 160
+    #      ridgewood += 220
+    #    end
+    #  }
+    #elsif customerType.eql?("Rewards")
+    #  days.each { |day|
+    #    if day.eql?("sat") || day.eql?("sun")
+    #      lakewood += 80
+    #      bridgewood += 50
+    #      ridgewood += 100
+    #    else
+    #      lakewood += 80
+    #      bridgewood += 110
+    #      ridgewood += 40
+    #    end
+    #  }
+    #end
+    #temp = (lakewood < bridgewood) ? lakewood : bridgewood
+    #totalPrice = temp < ridgewood ? temp : ridgewood
+    #if totalPrice.eql?(lakewood)
+    #  "Lakewood"
+    #elsif totalPrice.eql?(bridgewood)
+    #  "Bridgewood"
+    #elsif totalPrice.eql?(ridgewood)
+    #  "Ridgewood"
+    #end
   end
 end
